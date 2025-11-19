53a2a3d9e77a23fad9d62eee9830d9c2f1cca97a
amandusw <amandusw@outlook.com>
2025-11-19 13:37:50 +0100

    S ss

--- oppg2-1.php
+++ oppg2-1.php
@@ -12,7 +12,11 @@
     <a href="oppg2-2.html" style="color:white;margin-right:20px;">Oppgave 2-2</a>
     <a href="oppg2-3.html" style="color:white;margin-right:20px;">Oppgave 2-3</a>
     <a href="oppg2-4.html" style="color:white;margin-right:20px;">Oppgave 2-4</a>
-</nav>
+    <a href="oppg2-5.html" style="color:white;margin-right:20px;">Oppgave 2-5</a>
+    <a href="oppg2-6.html" style="color:white;margin-right:20px;">Oppgave 2-6</a>
+    <a href="oppg2-7.html" style="color:white;margin-right:20px;">Oppgave 2-7</a>
+    <a href="oppg2-8.html" style="color:white;margin-right:20px;">Oppgave 2-8</a>
+</nav>>
 
 <?php
     
